a7a1d7eade5c767154844cf9f1e343e2bfa7fda8
relictMarauder <relict_marauder@bonelis.de>
2016-04-02 10:48:56 +0200

    update repo

---
+++ create_repo.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\7-Zip\7z.exe" a -r  repo/plugin.video.relict.sovok.tv/plugin.video.relict.sovok.tv-%1.zip plugin.video.relict.sovok.tv/*  -x!.idea -x!*.iml -x!*.pyo
+cp plugin.video.relict.sovok.tv/changelog.txt repo/plugin.video.relict.sovok.tv/changelog-%1.txt
